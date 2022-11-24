89202c9ab9c87b180ee6be5297a149345d37b8a9
TheBiter <jefflee0712@gmail.com>
2022-11-23 17:43:29 -0800

    Push file deletions that are sometimes generated by unity renaming + source tree shenanigans.

--- Assets/Scripts/Data/SceneConstants.cs
+++
@@ -1,9 +0,0 @@
-namespace Data
-{
-    public class SceneConstants
-    {
-        public const string MainMenuScene = "MainMenuScene";
-        public const string GameplayScene = "GameplayScene";
-        public const string GameOverScene = "GameOverScene";
-    }
-}
